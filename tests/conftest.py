76e576dbb7808658b400ed08fce0b0e10940c54d
Harish Kumar <107610743+PrabhakarHarish@users.noreply.github.com>
2026-07-24 11:35:54 +0530

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,20 @@ from playwright.async_api import async_playwright
 @pytest_asyncio.fixture
 async def page():
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=False, slow_mo=300)
-        page = await browser.new_page()
-        yield page              # <-- the test runs at this point, using `page`
+        browser = await p.chromium.launch(headless=False)
+
+        context = await browser.new_context()
+
+        await context.tracing.start(
+            screenshots=True,
+            snapshots=True,
+            sources=True
+        )
+
+        page = await context.new_page()
+
+        yield page
+        await context.tracing.stop(path="trace.zip")
+
+        await context.close()
         await browser.close()
